623c1475e1e61711df380734100ef82de3dcee9c
Brandon <juhkim90@gmail.com>
2021-02-18 11:18:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,133 @@
+countdown = 10
+
+while (countdown > 0):
+	print (countdown)
+	countdown -= 1
+
+
+
+'''
+# Exercise 1
+# Expected outputs: 1x hello, 2x Python, 3x Evergreen, 1x 2021
+print("Exercise 1")
+student = 0
+
+while (student <= __):
+	if (student < __):
+		print ("hello")
+	elif (student < __):
+		print ("Python")
+	elif (student < __):
+		print ("bye")
+	elif (student <= __):
+		print ("Evergreen")
+	else:
+		print ("2021")
+
+	student += 1
+'''
+
+'''
+# Exercise 2
+# Expected outputs: 1x hello, 2x Python, 1x bye, 2x Evergreen, 1x 2021
+print("\nExercise 2")
+student = 5
+
+while (student <= __):
+	if (student < __):
+		print ("hello")
+	elif (student <= __):
+		print ("Python")
+	elif (student <= __):
+		print ("bye")
+	elif (student < __):
+		print ("Evergreen")
+	else:
+		print ("2021")
+
+	student += 1
+'''
+
+'''
+# Exercise 3
+# Expected outputs: 1x Python, 2x bye, 3x Evergreen, 4x 2021
+print("\nExercise 3")
+student = __
+
+while (student > 0):
+	if (student >= __):
+		print ("hello")
+	elif (student >= __):
+		print ("Python")
+	elif (student > __):
+		print ("bye")
+	elif (student >= __):
+		print ("Evergreen")
+	else:
+		print ("2021")
+
+	student -= 1
+'''
+
+'''
+# Answer to Exercise 1
+# Expected outputs: 1x hello, 2x Python, 3x Evergreen, 1x 2021
+print("Exercise 1")
+student = 0
+
+while (student <= 6):
+	if (student < 1):
+		print ("hello")
+	elif (student < 3):
+		print ("Python")
+	elif (student < 3):
+		print ("bye")
+	elif (student <= 5):
+		print ("Evergreen")
+	else:
+		print ("2021")
+
+	student += 1
+'''
+
+'''
+# Answer to Exercise 2
+# Expected outputs: 1x hello, 2x Python, 1x bye, 2x Evergreen, 1x 2021
+print("\nExercise 2")
+student = 5
+
+while (student <= 11):
+	if (student < 6):
+		print ("hello")
+	elif (student <= 7):
+		print ("Python")
+	elif (student <= 8):
+		print ("bye")
+	elif (student < 11):
+		print ("Evergreen")
+	else:
+		print ("2021")
+
+	student += 1
+'''
+
+'''
+# Answer to Exercise 3
+# Expected outputs: 1x Python, 2x bye, 3x Evergreen, 4x 2021
+print("\nExercise 3")
+student = 10
+
+while (student > 0):
+	if (student >= 11):
+		print ("hello")
+	elif (student >= 10):
+		print ("Python")
+	elif (student > 7):
+		print ("bye")
+	elif (student >= 5):
+		print ("Evergreen")
+	else:
+		print ("2021")
+
+	student -= 1
+'''
